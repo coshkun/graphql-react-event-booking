0fdde2fd616ff31e156c9f574d9bf38312c12db7
Coskun Caner <cskncnr@gmail.com>
2020-02-18 02:17:55 +0300

    RootSchema created & API tested first time

--- app.js
+++ app.js
@@ -4,12 +4,43 @@ let http = require('http')
   , app = express()
   , bodyParser = require('body-parser');
 
+let graphqlHttp = require('express-graphql')
+let { buildSchema } = require('graphql')
+
 app.use(bodyParser.json())
 
 
-app.get('/', (req, res, next) => {
-    res.status(200).send('Welcome, API v.1.0.0')
-})
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Romantic Cooking', 'Sailing', 'All Night Coding']
+        },
+        createEvent: (args) => {
+            let eventName = args.name
+            return eventName
+        }
+    },
+    graphiql: true // visual debuger tool
+}))
+
+
+// app.get('/', (req, res, next) => {
+//     res.status(200).send('Welcome, API v.1.0.0')
+// })
 
 
 //app.listen(3000)
